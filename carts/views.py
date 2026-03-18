e474517ef2848a6ab1bc7f2336356efc0823c316
Sanjana Ramgarhia <sanjanaramgarhia@gmail.com>
2026-03-18 18:15:37 +0530

    Add Variation model and fix admin registration

--- carts/views.py
+++ carts/views.py
@@ -12,6 +12,11 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
+    if request.method == 'POST':
+        color = request.POST['color']
+        size = request.POST['size']
+        print(color, size)
+        
     product = Product.objects.get(id=product_id) # get the product
     try:
         cart = Cart.objects.get(cart_id =_cart_id(request)) # get the cart using the card id used in session.
