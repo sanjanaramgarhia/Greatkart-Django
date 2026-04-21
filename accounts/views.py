e21144e59055d537564ada60320efb7f56bd72e8
Sanjana Ramgarhia <sanjanaramgarhia@gmail.com>
2026-04-21 17:50:43 +0530

    Cartitem modified

--- accounts/views.py
+++ accounts/views.py
@@ -57,7 +57,7 @@ def login(request):
         email = request.POST['email']
         password = request.POST['password']
 
-        user = auth.authenticate(username=email, password=password)
+        user = auth.authenticate(email=email, password=password)
 
         if user is not None:
             try:
@@ -66,12 +66,39 @@ def login(request):
                 if is_cart_item_exists:
                     cart_item = Cartitem.objects.filter(cart=cart)
 
+                    # Getting the product variations by cart id
+                    product_variation = []
                     for item in cart_item:
-                        item.user = user
-                        item.save()
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+                    # Get the cart items from the user to access his product variations
+                    cart_item = Cartitem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    # product_variation = [1, 2, 3, 4, 6]
+                    # ex_var_list = [4, 6, 3, 5]
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = Cartitem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = Cartitem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
             except:
                 pass
-
             auth.login(request, user)
             messages.success(request, 'You are now logged in.')
             return redirect('dashboard')
