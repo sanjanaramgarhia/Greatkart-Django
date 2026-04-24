578526e721d6980c217fcd07f85264d017266784
Sanjana Ramgarhia <sanjanaramgarhia@gmail.com>
2026-04-24 17:39:02 +0530

    Add Order tables

---
+++ orders/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from carts.models import Cartitem
+
+def place_order(request):
+    current_user = request.user
+
+    # If the cart count is less than or equal to 0, then redirect back to shop
+    cart_items = Cartitem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect('store')
