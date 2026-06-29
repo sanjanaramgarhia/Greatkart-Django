c8eb93c5368549fa02592aa9e20d575652e9c7a3
Sanjana Ramgarhia <sanjanaramgarhia@gmail.com>
2026-06-29 12:50:25 +0530

    Brevo Email verification

--- greatcart/settings.py
+++ greatcart/settings.py
@@ -147,13 +147,16 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
+# Brevo configuration
+SITE_ID = 1
+
 # SMTP configuration
-EMAIL_BACKEND = config('EMAIL_BACKEND')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+# EMAIL_BACKEND = config('EMAIL_BACKEND')
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_PORT = config('EMAIL_PORT', cast=int)
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 
 STORAGES = {
     "default": {
